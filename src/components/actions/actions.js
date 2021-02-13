c8c4587acb5676a37b8e253deab3b2fd147fb8b5
williamAstudillo <wsastudilloa@gmail.com>
2021-02-13 11:45:32 -0500

    api

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function getCities(ciudad) {
     return function (dispatch) {
-        return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=4ae2636d8dfbdc3044bede63951a019b`)
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=4ae2636d8dfbdc3044bede63951a019b`)
             .then(json => {
                 console.log(json.data)
                 dispatch({ 
